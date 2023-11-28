da9c486f57196307e5772dcef251e98a78aab20d
pbadey <63054435+pbadey@users.noreply.github.com>
2023-11-28 16:35:46 +0100

    Init react-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenue sur la page d'accueil de l'application TODO ! 
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
