004429403970582ae8599a0940444292bcd33e44
pbadey <63054435+pbadey@users.noreply.github.com>
2023-12-11 11:33:52 +0100

    Todo app first render

---
+++ src/Component/table.js
@@ -0,0 +1,27 @@
+export default function Table({ tasks, modifyTask, deleteTask }) {
+    return (
+        <table className="table">
+            <thead className="thead">
+                <tr>
+                    <th>Titre</th>
+                    <th>Description</th>
+                    <th>Statut</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map((task) => (
+                    <tr key={task.id}>
+                        <td>{task.title}</td>
+                        <td>{task.description}</td>
+                        <td>{task.status}</td>
+                        <td>
+                            <button className='liteButton' onClick={modifyTask(task.id)}>Modifier</button>
+                            <button className='liteButton' onClick={deleteTask(task.id)}>Supprimer</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
