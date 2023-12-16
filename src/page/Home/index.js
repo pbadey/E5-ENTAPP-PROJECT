90c4870cfa900d4ab0c76139068cb269fe9b743b
pbadey <63054435+pbadey@users.noreply.github.com>
2023-12-16 16:08:44 +0100

    Try co communicate with API

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,56 +1,37 @@
 import { useEffect, useState } from 'react';
 import './home.css';
-import API_Manager from '../../services/API_Manager';
 import Row from '../../Component/row';
+import { addTodo, getTodos } from '../../services/api';
 
 export default function HomePage() {
-  const [todos, setTodos] = useState([]);
+ const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    API_Manager.getTodos()
+ useEffect(() => {
+    getTodos()
       .then((response) => {
         setTodos(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+ }, []);
 
-  function addTodo() {
-    let text = prompt('Nouvelle tâche');
-    let todo = {
-      text,
-      status: false,
-    };
-
-    API_Manager.addTodo(todo)
-      .then((response) => {
-        setTodos([...response.data]);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+ async function newTodo() {
+  let title = prompt('Nouvelle tâche');
+  let todo = {
+     id: todos.length + 1,
+     title,
+     status: 'A faire',
+  };
+  try {
+     const response = await addTodo(todo);
+     setTodos([...todos, response.data]);
+  } catch (error) {
+     console.log(error);
   }
+ }
 
-  const tasks = [
-    {
-      id: 1,
-      title: 'Ma tâche',
-      status: false,
-    },
-    {
-      id: 2,
-      title: 'Ma tâche 2',
-      status: false,
-    },
-    {
-      id: 3,
-      title: 'Ma tâche 3',
-      status: false,
-    },
-    
-  ]
-  return (
+ return (
     <div className="App">
       <header className="App-header">
         <p>
@@ -58,24 +39,23 @@ export default function HomePage() {
         </p>
 
       </header>
-      <button className='CreateNewTask' onClick={addTodo}> Nouvelle tâche </button>
+      <button className='CreateNewTask' onClick={newTodo}> Nouvelle tâche </button>
 
       <table className="table">
             <thead className="thead">
                 <tr>
-                    <th>Terminer</th>
                     <th>Titre</th>
                     <th>Statut</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
-                {tasks.map((task) => (
+                {todos.map((task) => (
                     <Row key={task.id} object={task} />
                 ))}
             </tbody>
         </table>
   
     </div>
-  );
+ );
 }
